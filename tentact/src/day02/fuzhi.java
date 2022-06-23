ec7c8c0b06a1f03591a48009d67d938488ef33ed
GTlht <1504090031@qq.com>
2022-06-23 13:45:07 +0800

    控制台提交测试

---
+++ tentact/src/day02/fuzhi.java
@@ -0,0 +1,14 @@
+package day02;
+
+import java.util.Scanner;
+
+public class fuzhi {
+	public static void (String[] args) {
+		
+		int num=1234;
+		System.out.println(num);
+		Scanner scanner=new Scanner(System.in);
+		int number=scanner.nextInt();
+	}
+
+}
